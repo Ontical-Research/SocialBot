73138b0ab23548b522b17533faf2eb904f071f73
W.P. McNeill <billmcn@gmail.com>
2026-02-26 13:27:00 -0600

    feat(#26): add /bot route with BotApp placeholder

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import BotApp from "./BotApp.tsx";
+
+const isBotRoute = window.location.pathname.startsWith("/bot");
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <StrictMode>{isBotRoute ? <BotApp /> : <App />}</StrictMode>,
 );
